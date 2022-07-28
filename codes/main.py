1a3d0d937719f5910192d79b15003e711c7ff0d2
dylanwooch <dylan.wooch@tindogroup.com>
2022-07-28 17:17:33 +0800

    update 28/07/2022

--- codes/main.py
+++ codes/main.py
@@ -9,47 +9,48 @@ instanceid = getPending()
 for x in range(len(instanceid)):
 
     ec2id = instanceid[x][0]
-    
+    #print(ec2id)
     #Get CyberArk ID from database
     cyberarkid = getID(ec2id)
+    #print(cyberarkid)
 
-   
+    #Login to CyberArk
+    SESSIONID = loginUser() #Store Session ID to global variable
+    SESSIONHEADER = {"content-type":"application/json", 'Authorization' : SESSIONID}
     
     for i in range(len(cyberarkid)):
         caid= cyberarkid[i][0]
         
-        #Login to CyberArk
-        SESSIONID = loginUser() #Store Session ID to global variable
-        SESSIONHEADER = {"content-type":"application/json", 'Authorization' : SESSIONID}
-
         #Send API to get JSON using CyberArk ID (caid)
         result = sendapiRequest(UPDATEACCOUNTURL.format(caid), "",SESSIONHEADER, "GET")
         
+        #declare variable
+        id = result['id']
+        tries = getTries(id)
+        isSuccess = False
+        logger("Going in while loop")
 
-        while not isSuccess and tries[0][0] < 5:
-            #declare variable
-            id = result['id']
-            role = getRoles(id)
-            tries = getTries(id)
-            isSuccess = False
-            
+        while not isSuccess and (tries[0][0]) < 5:   
             if "status" in result['secretManagement']:
                 if result['secretManagement']['status'] == "success":
                     updateStatus(id)
-                    updateTries(id)
+                    tries = updateTries(id)
+                    print("Updated tries: " + tries)
                     isSuccess = True
                     logger(id + " successfully updated ")
                     break
                 elif result['secretManagement']['status'] == "failure":
+                    role = getRoles(id)
                     if role == "admin" or "logon":
                         sendapiRequest(CHANGEURL.format(id),"",SESSIONHEADER, "POST")
                         updateTries(id)
+                        print("Updated tries: " + tries)
                     elif role == "user":
                         sendapiRequest(RECONCILEURL.format(id),"",SESSIONHEADER, "POST")
                         updateTries(id)
-                print(f"tries {tries[0][0]}")
+                        print("Updated tries: " + tries)
             else:
-                ("Please wait for account status")
+                print("Please wait for account status")
                 time.sleep(60)
 
                 #Send API to get JSON using CyberArk ID (caid)
---
+++ codes/test.py
@@ -0,0 +1,31 @@
+from cyberark_auth import *
+from database import *
+
+#Get instance ID from database
+instanceid = getPending()
+
+for x in range(len(instanceid)):
+
+    ec2id = instanceid[x][0]
+    #print(ec2id)
+    #Get CyberArk ID from database
+    cyberarkid = getID(ec2id)
+    #print(cyberarkid)
+
+    #Login to CyberArk
+    SESSIONID = loginUser() #Store Session ID to global variable
+    SESSIONHEADER = {"content-type":"application/json", 'Authorization' : SESSIONID}
+    
+    for i in range(len(cyberarkid)):
+        caid= cyberarkid[i][0]
+        
+        #Send API to get JSON using CyberArk ID (caid)
+        result = sendapiRequest(UPDATEACCOUNTURL.format(caid), "",SESSIONHEADER, "GET")
+        
+        #declare variable
+        id = result['id']
+        tries = getTries(id)
+        print(len(tries))
+        print(tries[0][0])
+        updateTries(id)
+        print(tries[0][0])
